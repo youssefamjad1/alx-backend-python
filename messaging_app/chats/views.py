3b57ba8bf335ca0d7e9ca708a9b7f05a226acf72
Youssef HIMMI <yhimmi1@gmail.com>
2025-05-29 21:57:07 +0200

    Add viewsets with filters and status; set up router in chats/urls.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
@@ -9,8 +11,22 @@ class ConversationViewSet(viewsets.ModelViewSet):
     search_fields = ['users__first_name', 'users__last_name']  # example search by user names
     ordering_fields = ['created_at']
 
+    # Example override to demonstrate using status
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['sent_at']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
