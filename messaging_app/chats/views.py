f5a21eec3ca08fd95ad1e3d06d62209f8579e4fd
Youssef HIMMI <yhimmi1@gmail.com>
2025-05-29 21:53:20 +0200

    Add routers and URL configurations for chats app; implement Conversation and Message viewsets

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,27 +1,16 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework import viewsets, filters
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-
-    # Override create method to handle new conversations
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['users__first_name', 'users__last_name']  # example search by user names
+    ordering_fields = ['created_at']
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-
-    # Override create method to send messages to a conversation
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['sent_at']
