0035d8b696bc2d39259328ee87380b31c3f8fc52
Youssef HIMMI <yhimmi1@gmail.com>
2025-06-15 20:24:20 +0200

    changes

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,5 +1,1 @@
-from django.contrib import admin
-from .models import Message, Notification
-
-admin.site.register(Message)
-admin.site.register(Notification)
+# admin.py
--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -1,8 +1,1 @@
-from django.apps import AppConfig
-
-class MessagingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messaging'
-
-    def ready(self):
-        import messaging.signals
+# apps.py
